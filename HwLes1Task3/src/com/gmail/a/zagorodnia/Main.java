03eb19bcdb8beb60ff8f28e86ace679bcd5bd22c
Alina Ihnatiuk <Ihnatiuk.a@gmail.com>
2018-02-24 23:17:05 +0200

    sec commit

--- HwLes1Task3/src/com/gmail/a/zagorodnia/Main.java
+++ HwLes1Task3/src/com/gmail/a/zagorodnia/Main.java
@@ -5,16 +5,16 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Vector3d vectorOne  = new Vector3d(18, 11, 12.5);
-        Vector3d vectorTwo = new Vector3d(26, 14, 9.5);
-        Vector3d vectorThree = vectorOne.vectAddition(vectorTwo);
-        Vector3d vectorFour = vectorOne.scalarProd(vectorTwo);
-        Vector3d vectorFive = vectorOne.vectorProd(vectorTwo);
-
-        System.out.println(vectorOne);
-        System.out.println(vectorTwo);
-        System.out.println(vectorThree);
-        System.out.println(vectorFour);
-        System.out.println(vectorFive);
+		Vector3d vectorTwo = new Vector3d(26, 14, 9.5);
+		Vector3d vectorThree = vectorOne.vectAddition(vectorTwo);
+		Vector3d vectorFour = vectorOne.scalarProd(vectorTwo);
+		Vector3d vectorFive = vectorOne.vectorProd(vectorTwo);
+		
+		System.out.println(vectorOne);
+		System.out.println(vectorTwo);
+		System.out.println(vectorThree);
+		System.out.println(vectorFour);
+		System.out.println(vectorFive);
        
 	}
 
--- HwLes1Task3/src/com/gmail/a/zagorodnia/Vector3d.java
+++ HwLes1Task3/src/com/gmail/a/zagorodnia/Vector3d.java
@@ -3,57 +3,57 @@ package com.gmail.a.zagorodnia;
 public class Vector3d {
 	
 	private double x;
-    private double y;
-    private double z;
+	private double y;
+	private double z;
 
-    public Vector3d(double x, double y, double z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-    }
+	public Vector3d(double x, double y, double z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
 
-    public Vector3d() {
-    }
+	public Vector3d() {
+	}
 
-    public double getX() {
-        return x;
-    }
+	public double getX() {
+		return x;
+	}
 
-    public void setX(double x) {
-        this.x = x;
-    }
+	public void setX(double x) {
+		this.x = x;
+	}
+	
+	public double getY() {
+		return y;
+	}
 
-    public double getY() {
-        return y;
-    }
+	public void setY(double y) {
+		this.y = y;
+	}
 
-    public void setY(double y) {
-        this.y = y;
-    }
-
-    public double getZ() {
-        return z;
-    }
-
-    public void setZ(double z) {
-        this.z = z;
-    }
-
-    public Vector3d vectAddition(Vector3d vector) {
-        return new Vector3d(x + vector.x, y + vector.y, z + vector.z);
-    }
-
-    public Vector3d scalarProd(Vector3d vector) {
-        return new Vector3d(x * vector.x, y * vector.y, z * vector.z);
-    }
-
-    public Vector3d vectorProd(Vector3d vector) {
+	public double getZ() {
+		return z;
+	}
+	
+	public void setZ(double z) {
+		this.z = z;
+	}
+	
+	public Vector3d vectAddition(Vector3d vector) {
+		return new Vector3d(x + vector.x, y + vector.y, z + vector.z);
+	}
+	
+	public Vector3d scalarProd(Vector3d vector) {
+		return new Vector3d(x * vector.x, y * vector.y, z * vector.z);
+	}
+	
+	public Vector3d vectorProd(Vector3d vector) {
+		
+		return new Vector3d(y * vector.z - z * vector.y, z * vector.x - x * vector.z, x * vector.y - y * vector.x);
     	
-    	return new Vector3d(y * vector.z - z * vector.y, z * vector.x - x * vector.z, x * vector.y - y * vector.x);
+//		return new Vector3d(z * vector.y - y * vector.z, x * vector.z - z * vector.x, y * vector.x - x * vector.y);
     	
-//    	return new Vector3d(z * vector.y - y * vector.z, x * vector.z - z * vector.x, y * vector.x - x * vector.y);
-    	
-    }
+	}
         
 	@Override
 	public String toString() {
